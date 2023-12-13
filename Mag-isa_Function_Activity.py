116e9279c07fc906a353d1dd03ea55a49f174680
AdaCompProg1371 <144907186+AdaCompProg1371@users.noreply.github.com>
2023-12-13 23:40:33 +0800

    Update Mag-isa_Function_Activity.py

--- Mag-isa_Function_Activity.py
+++ Mag-isa_Function_Activity.py
@@ -1,65 +1,22 @@
-def number():
+def solving_sum(a, b, c):
+    total = a + b + c
 
-    for i in range(0, a_1):
-
-
-        for j in range(0, b_2):
-
-
-            for k in range(0, c_3):
-
-                result = 0
-
-            return result
-
-
-def a_1():
-    a_1 = eval(input("Enter first number:"))
-
-
-def b_2():
-    b_2 = eval(input("Enter second number:"))
-
-
-def c_3():
-    c_3 = eval(input("Enter third number:"))
-
-
-def answer():
-    if a_1 != b_2 and b_2 != c_3:
-        formula = a_1 + b_2 + c_3
-    elif a_1 == b_2 and a_1 != c_3:
-        formula = a_1 * b_2 + c_3
-    elif a_1 != b_2 and a_1 == c_3:
-        formula = a_1 + b_2 * c_3
-    elif b_2 == c_3 and a_1 != c_3:
-        formula = a_1 + b_2 * c_3
-    elif b_2 != c_3 and a_1 == c_3:
-        formula = a_1 * c_3 + b_2
+    if a == b == c:
+        return a * b * c
+    elif a == c:
+        return a * c + b
+    elif b == c:
+        return b * c + a
+    elif a == b:
+        return a * b + c
     else:
-        formula = a_1 * b_2 * c_3
+        return total
 
+print("")
+a = eval(input("Enter first number:"))
+b = eval(input("Enter second number:"))
+c = eval(input("Enter third number:"))
 
-def main():
-
-    a_1 = eval(input("Enter first number:"))
-
-    b_2 = eval(input("Enter second number:"))
-
-    c_3 = eval(input("Enter third number:"))
-
-
-main()
-a_1()
-b_2()
-c_3()
-answer()
-number()
-
-# Type three numbers
-a_1 = eval(input("Enter first number:"))
-b_2 = eval(input("Enter second number:"))
-c_3 = eval(input("Enter third number:"))
-
-# Answers based on the typed numbers
-print("The answer for the given numbers is:", answer)
+answer = solving_sum(a, b, c)
+print("")
+print("The answer to the given three numbers will be: ", answer)
